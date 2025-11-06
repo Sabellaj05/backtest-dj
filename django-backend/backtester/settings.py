697a172822f62b12ed7423fce96784558e353aab
macsaens <thesaens@proton.me>
2025-11-06 17:05:31 -0300

    fix: back to sqlite, duckdb not supported

--- django-backend/backtester/settings.py
+++ django-backend/backtester/settings.py
@@ -75,8 +75,8 @@ WSGI_APPLICATION = 'backtester.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'duckdb_engine',
-        'NAME': BASE_DIR / 'db.duckdb',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
